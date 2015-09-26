93ec2aec82d45107cda2c86527a3689b08c18e20
soltisclan <zoltiz@gmail.com>
2015-09-26 15:11:47 -0400

    Adding favicon.ico and streaming code for it.

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-var http = require('http');
-http.createServer(function (req, res) {
+var http = require('http'),
+    fileSystem = require('fs'),
+    path = require('path');
+http.createServer(reqprocessor).listen(8080);
+
+function reqprocessor(req, res) {
     console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Cat and Azure Web Apps!</h1>');
-}).listen(process.env.PORT);
+    if (req.url == '/favicon.ico')
+    {
+        var filePath = path.join(__dirname, 'favicon.ico');
+        var stat = fileSystem.statSync(filePath);
+        res.writeHead(200, {
+            'Content-Type': 'image/x-icon',
+            'Content-Length': stat.size
+        });
+
+        var readStream = fileSystem.createReadStream(filePath);
+        // We replaced all the event handlers with a simple call to readStream.pipe()
+        readStream.pipe(res);
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Hello Cat and Azure Web Apps!</h1>');    
+    }
+}
