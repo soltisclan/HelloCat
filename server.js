2545fa163c15d12b8789ddc0fe15e32d38eaf386
soltisclan <zoltiz@gmail.com>
2015-09-29 11:06:17 -0400

    Cleaned up for JSLint

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-var http = require('http'),
-    express = require('express'),
-    app = express(),
-    fileSystem = require('fs'),
-    path = require('path');
-
-app.get('/',reqprocessor_ex);
-app.use(express.static('public'));
-
+var express = require('express'),
+    app = express();
+    
 function reqprocessor_ex(req,res){
     res.send('<h3>Hail Cats!</h3>');
 }   
 
-var server = app.listen(process.env.PORT || 8080);
+app.get('/',reqprocessor_ex);
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 8080);
